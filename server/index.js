7fcccfeba2dae6faf5329f894b23d69a661a3f8b
Anjali <k.anjaliii.1011@gmail.com>
2025-02-11 10:57:16 +0530

    Fix server path and deployment config

---
+++ server/index.js
@@ -0,0 +1,14 @@
+
+const { app, server } = require('./server');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Fleet Battle Server is running! 🚀');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
