03c27051fac37ad12d0667144f5d60bd8dbca99b
Anjali <k.anjaliii.1011@gmail.com>
2025-02-11 09:57:43 +0530

    Added default route for backend

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
-const { server } = require('./server');
+const { app,server } = require('./server');
 
+require('dotenv').config();
 const PORT = process.env.PORT || 5000;
-
+app.get('/', (req, res) => {
+    res.send('Fleet Battle Server is running! 🚀');
+  });
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
